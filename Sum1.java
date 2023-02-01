4e414c586058c44af144249b39163a1cbdb97ccc
DataMonarch <toghrultahirov2@gmail.com>
2023-02-01 22:40:49 +0400

    latest updates

--- Sum1.java
+++ Sum1.java
@@ -3,7 +3,7 @@ public class Sum1 {
     public static void main(String[] args) {
         int a = Integer.parseInt(args[0]);
         long b = Long.parseLong(args[1]);
-        float sum = a + b;
+        long sum = a + b;
 
         System.out.println(a + " + " + b + " = " + sum);
     }
--- test.java
+++ test.java
@@ -1,3 +1,12 @@
 public class test {
+
+    public static void main(String[] args){
+        String name = args[0];
+        System.out.println("Hello, " + name);
+    }
+
+}
+
+public class test2: test {
     
 }
